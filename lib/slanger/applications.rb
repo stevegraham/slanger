2b75144e756bbc5427b074e0ba3a72b835b73f08
Gilbert Roulot <gilbert.roulot@tech-angels.fr>
2012-04-06 12:50:47 +0200

    Optionnal use of MongoDB to get applications.
    If mongodb is enabled, applications will be pulled from its
    slanger.applications collection.

--- lib/slanger/applications.rb
+++ lib/slanger/applications.rb
@@ -8,24 +8,98 @@ module Slanger
     def_delegators :apps, :[], :[]=
 
     def apps
-      @applications ||= {"bob2" => Application.new("bob2", "91e1c10e7583bee5d236", "3856be38afd611bf37a7") }
+      # In memory database of applications
+      @applications ||= {}
     end
     
+    def apps_by_key
+      @applications_by_key ||= {}
+    end
+
     def by_id(id)
       apps[id]
     end
 
     def by_key(key)
-      apps.each{|k, application| return application if application.key == key}
-      nil
+      apps_by_key[key]
     end
 
     def add(id, key, secret)
-      apps[id] = Application.new(id, key, secret)
+      app = Application.new(id, key, secret)
+      apps[id] = app
+      apps_by_key[app.key] = app
       Logger.info("Created application " + id)
       Logger.audit("Created application " + id)
     end
   end
 
   Applications = ApplicationsSingleton.instance
+
+  if Config.mongo
+    # Use mongo db to store and retrieve applications
+    module ApplicationsMongoDBAspect
+      include Aquarium::DSL
+
+      around :calls_to => :by_id, :for_object => Slanger::Applications do |join_point, applicationssingleton, *args|
+        # Get result
+        Fiber.new do
+          result = join_point.proceed
+          if result.nil?
+            # No result, try looking into the Database
+            app_id = args[0]
+            result = get(_id: app_id)
+          end
+          # Add to memory db
+          unless result.nil?
+            Applications.apps[result.id] = result
+            Applications.apps_by_key[result.key] = result
+          end
+          result
+        end.resume
+      end
+
+      around :calls_to => :by_key, :for_object => Slanger::Applications do |join_point, applicationssingleton, *args|
+        # Get result
+        Fiber.new do
+          result = join_point.proceed
+          if result.nil?
+            # No result, try looking into the Database
+            app_key = args[0]
+            result = get(key: app_key)
+          end
+          # Add to memory db
+          unless result.nil?
+            Applications.apps[result.id] = result
+            Applications.apps_by_key[result.key] = result
+          end
+          result
+        end.resume
+      end
+
+      # Retrieve an application from the database
+      def get(conditions)
+        f = Fiber.current
+        result = applications.find_one(conditions)
+        result.callback do |doc|
+          # The application was found
+          f.resume doc
+        end
+        result.errback do |err|
+          # Error
+          Logger.error "Error when retrieving application from database: " + err.to_s
+          f.resume nil
+        end
+        doc = Fiber.yield
+        doc && Application.new(doc['_id'], doc['key'], doc['secret'])
+      end
+
+      # Application collection in Mongodb
+      def applications
+        @applications ||= Mongo.collection("slanger.applications")
+      end
+      extend self
+    end
+  end
 end
+
+
