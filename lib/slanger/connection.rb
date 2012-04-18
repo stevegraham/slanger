26b4030e53589ed87f6efcf0157e4ac27f84c349
Mark Burns <markthedeveloper@gmail.com>
2012-04-18 09:07:47 +0100

    moving some of the methods from the subscription onto the connection

--- lib/slanger/connection.rb
+++ lib/slanger/connection.rb
@@ -6,8 +6,18 @@ module Slanger
       @socket, @socket_id = socket, socket_id
     end
 
+    def send_message m
+      msg = JSON.parse m
+      s = msg.delete 'socket_id'
+      socket.send msg.to_json unless s == socket_id
+    end
+
     def send_payload *args
       socket.send format(*args)
+    end
+
+    def error e
+      send_payload nil, 'pusher:error', e
     end
 
     def establish
@@ -15,12 +25,10 @@ module Slanger
       send_payload nil, 'pusher:connection_established', { socket_id: @socket_id }
     end
 
+    private
+
     def format(channel_id, event_name, payload = {})
       { channel: channel_id, event: event_name, data: payload }.to_json
-    end
-
-    def error e
-      send_payload nil, 'pusher:error', e
     end
   end
 end
--- lib/slanger/subscription.rb
+++ lib/slanger/subscription.rb
@@ -1,6 +1,7 @@
 module Slanger
   class Subscription
     attr_accessor :connection, :socket
+    delegate :send_payload, :send_message, :error, :socket_id, to: :connection
 
     def initialize socket, socket_id, msg
       @connection = Connection.new socket, socket_id
@@ -8,18 +9,12 @@ module Slanger
     end
 
     def handle
-      connection.send_payload channel_id, 'pusher_internal:subscription_succeeded'
+      send_payload channel_id, 'pusher_internal:subscription_succeeded'
 
       channel.subscribe { |m| send_message m }
     end
 
     private
-
-    def send_message m
-      msg = JSON.parse(m)
-      s = msg.delete 'socket_id'
-      connection.socket.send msg.to_json unless s == connection.socket_id
-    end
 
     def channel
       Channel.from channel_id
@@ -30,19 +25,27 @@ module Slanger
     end
 
     def token(channel_id, params=nil)
-      string_to_sign = [connection.socket_id, channel_id, params].compact.join ':'
+      string_to_sign = [socket_id, channel_id, params].compact.join ':'
       HMAC::SHA256.hexdigest(Slanger::Config.secret, string_to_sign)
     end
 
     def invalid_signature?
-      token(channel_id, @msg['data']['channel_data']) != @msg['data']['auth'].split(':')[1]
+      token(channel_id, data) != auth.split(':')[1]
+    end
+
+    def auth
+      @msg['data']['auth']
+    end
+
+    def data
+      @msg['data']['channel_data']
     end
 
     def handle_invalid_signature
       message = "Invalid signature: Expected HMAC SHA256 hex digest of "
-      message << "#{connection.socket_id}:#{@msg['data']['channel']}, but got #{@msg['data']['auth']}"
+      message << "#{socket_id}:#{channel_id}, but got #{auth}"
 
-      connection.error({ message: message})
+      error({ message: message})
     end
   end
 end
