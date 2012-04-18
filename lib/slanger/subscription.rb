d48a49321e4b318156de3f7ebc2ba12314517672
Mark Burns <markthedeveloper@gmail.com>
2012-04-18 07:56:30 +0100

    token method doesn't need params

--- lib/slanger/subscription.rb
+++ lib/slanger/subscription.rb
@@ -29,13 +29,17 @@ module Slanger
       @msg['data']['channel']
     end
 
-    def token(channel_id, params=nil)
-      string_to_sign = [payload.socket_id, channel_id, params].compact.join ':'
-      HMAC::SHA256.hexdigest(Slanger::Config.secret, string_to_sign)
+    def token
+      to_sign = [connection.socket_id, channel_id].compact.join ':'
+      HMAC::SHA256.hexdigest(Slanger::Config.secret, to_sign)
     end
 
     def invalid_signature?
-      token(channel_id, @msg['data']['channel_data']) != @msg['data']['auth'].split(':')[1]
+      token != auth.split(':')[1]
+    end
+
+    def auth
+      @msg['data']['auth']
     end
 
     def handle_invalid_signature
