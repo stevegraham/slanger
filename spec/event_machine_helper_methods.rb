33ec4370bb0da9069c09942fb39a1cafdd71a7ce
Stevie Graham <sjtgraham@mac.com>
2012-04-27 17:56:41 +0200

    Revert "rescue specific exception". Causing intermittent failures on Travis, which wait_for_slanger was intended to fix.
    This reverts commit ea4a67980123ee519247cfe71a25a323db804e01.

--- spec/event_machine_helper_methods.rb
+++ spec/event_machine_helper_methods.rb
@@ -30,7 +30,7 @@ module EventMachineHelperMethods
     opts = { port: 8080 }.update opts
     begin
       TCPSocket.new '0.0.0.0', opts[:port]
-    rescue SystemCallError
+    rescue
       sleep 0.1
       retry
     end
