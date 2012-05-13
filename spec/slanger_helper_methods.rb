b15cc66cf7629ed7b5472017adeab4183a735f01
Mark Burns <markthedeveloper@gmail.com>
2012-05-13 21:30:25 +0100

    rather than just EventMachineHelperMethods these are more Slanger related

--- spec/slanger_helper_methods.rb
+++ spec/slanger_helper_methods.rb
@@ -1,5 +1,4 @@
-module EventMachineHelperMethods
-
+module SlangerHelperMethods
   def start_slanger_with_options options={}
     # Fork service. Our integration tests MUST block the main thread because we want to wait for i/o to finish.
     @server_pid = EM.fork_reactor do
@@ -75,8 +74,8 @@ module EventMachineHelperMethods
   end
 
   def auth_from options
-    id = options[:message]['data']['socket_id']
-    name = options[:name]
+    id      = options[:message]['data']['socket_id']
+    name    = options[:name]
     user_id = options[:user_id]
     Pusher['presence-channel'].authenticate(id, {user_id: user_id, user_info: {name: name}})
   end
@@ -94,70 +93,4 @@ module EventMachineHelperMethods
                auth: auth } }.to_json)
 
   end
-
-  class HaveAttributes
-    attr_reader :messages, :attributes
-    def initialize attributes
-      @attributes = attributes
-    end
-
-    CHECKS = %w(first_event last_event last_data )
-
-    def matches?(messages)
-      @messages = messages
-      @failures = []
-
-      check_connection_established if attributes[:connection_established]
-      check_id_present             if attributes[:id_present]
-
-      CHECKS.each { |a| attributes[a.to_sym] ?  check(a) : true }
-
-      @failures.empty?
-    end
-
-    def check message
-      send(message) == attributes[message.to_sym] or @failures << message
-    end
-
-    def failure_message
-      @failures.map {|f| "expected #{f}: to equal #{attributes[f]} but got #{send(f)}"}.join "\n"
-    end
-
-    private
-
-    def check_connection_established
-      if first_event != 'pusher:connection_established'
-        @failures << :connection_established
-      end
-    end
-
-    def check_id_present
-      if messages.first['data']['socket_id'] == nil
-        @failures << :id_present
-      end
-    end
-
-    def first_event
-      messages.first['event']
-    end
-
-    def last_event
-      messages.last['event']
-    end
-
-    def last_data
-      messages.last['data']
-    end
-
-    def count
-      messages.length
-    end
-  end
-
-  def have_attributes attributes
-    HaveAttributes.new attributes
-  end
 end
-
-
-
