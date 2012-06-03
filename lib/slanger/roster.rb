822f430d9cb20121b259787e76d92f7546fe1575
Mark Burns <markthedeveloper@gmail.com>
2012-06-03 19:37:20 +0100

    moving Roster into Slanger namespace - whoops

--- lib/slanger/roster.rb
+++ lib/slanger/roster.rb
@@ -1,63 +1,63 @@
-class Roster
-  def initialize channel_id
-    @channel_id = channel_id
-  end
+module Slanger
+  class Roster
+    def initialize channel_id
+      @channel_id = channel_id
+    end
 
-  def subscribe channel_data
-    public_subscription_id = SecureRandom.uuid
+    def subscribe channel_data
+      public_subscription_id = SecureRandom.uuid
 
-    add public_subscription_id, channel_data
+      add public_subscription_id, channel_data
 
-    publisher = publish_connection_notification(
-      subscription_id: public_subscription_id,
-      online: true,
-      channel_data: channel_data,
-      channel: channel_id)
+      publisher = publish_connection_notification(
+        subscription_id: public_subscription_id,
+        online: true,
+        channel_data: channel_data,
+        channel: channel_id)
 
-    return publisher, public_subscription_id
-  end
+        return publisher, public_subscription_id
+    end
 
-  def unsubscribe public_subscription_id
-    remove public_subscription_id
+    def unsubscribe public_subscription_id
+      remove public_subscription_id
 
-    publish_connection_notification(
-      subscription_id: public_subscription_id,
-      online: false,
-      channel: channel_id)
-  end
+      publish_connection_notification(
+        subscription_id: public_subscription_id,
+        online: false,
+        channel: channel_id)
+    end
 
-  def get
-    fiber do |f|
-      Slanger.read_all(channel_id).callback { |res| f.resume res }
+    def get
+      fiber do |f|
+        Slanger.read_all(channel_id).callback { |res| f.resume res }
+      end
+    end
+
+    def add public_subscription_id, uuid
+      Slanger.set(channel_id, public_subscription_id, uuid)
+    end
+
+    def remove public_subscription_id
+      Slanger.delete(channel_id, public_subscription_id)
+    end
+
+    private
+
+    def publish_connection_notification(payload, retry_count=0)
+      # Send a subscription notification to the global slanger:connection_notification
+      # channel.
+      Slanger.publish('slanger:connection_notification', payload.to_json).
+        tap { |r| r.errback { publish_connection_notification payload, retry_count.succ unless retry_count == 5 } }
+    end
+
+    attr_reader :channel_id
+
+
+    def fiber
+      Fiber.new do
+        yield Fiber.current
+        Fiber.yield
+      end.resume
     end
   end
-
-  def add public_subscription_id, uuid
-    Slanger.set(channel_id, public_subscription_id, uuid)
-  end
-
-  def remove public_subscription_id
-    Slanger.delete(channel_id, public_subscription_id)
-  end
-
-  private
-
-  def publish_connection_notification(payload, retry_count=0)
-    # Send a subscription notification to the global slanger:connection_notification
-    # channel.
-    Slanger.publish('slanger:connection_notification', payload.to_json).
-      tap { |r| r.errback { publish_connection_notification payload, retry_count.succ unless retry_count == 5 } }
-  end
-
-  attr_reader :channel_id
-
-
-  def fiber
-    Fiber.new do
-      yield Fiber.current
-      Fiber.yield
-    end.resume
-  end
 end
-
-
