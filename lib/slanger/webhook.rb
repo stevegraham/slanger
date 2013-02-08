0d8c165bfca4d7967268d44d986b61b71830eaa6
Stevie Graham <sjtgraham@mac.com>
2013-02-08 19:28:37 +0000

    remove fiber from webhook. we do not use the request response anyway

--- lib/slanger/webhook.rb
+++ lib/slanger/webhook.rb
@@ -13,15 +13,9 @@ module Slanger
       digest   = OpenSSL::Digest::SHA256.new
       hmac     = OpenSSL::HMAC.hexdigest(digest, Slanger::Config.secret, payload)
 
-      Fiber.new do
-        f = Fiber.current
-        
-        EM::HttpRequest.new(Slanger::Config.webhook_url).
-          post(body: payload, head: { "X-Pusher-Key" => Slanger::Config.app_key, "X-Pusher-Secret" => hmac }).
-          callback { f.resume }
-          # TODO: Exponentially backed off retries for errors
-        Fiber.yield
-      end.resume
+      EM::HttpRequest.new(Slanger::Config.webhook_url).
+        post(body: payload, head: { "X-Pusher-Key" => Slanger::Config.app_key, "X-Pusher-Secret" => hmac })
+        # TODO: Exponentially backed off retries for errors
     end
 
     extend self
