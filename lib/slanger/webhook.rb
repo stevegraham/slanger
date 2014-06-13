b2d6decb8e202e6ffcbdbef54ff09ea1c43a837f
Andrey Subbotin <andrey@subbotin.me>
2014-06-13 15:38:31 +1000

    Rename the X-Pusher-Secret header to X-Pusher-Signature and supply the correct content type of 'application/json' on webhook requests.

--- lib/slanger/webhook.rb
+++ lib/slanger/webhook.rb
@@ -10,11 +10,16 @@ module Slanger
         time_ms: Time.now.strftime('%s%L'), events: [payload]
       }.to_json
 
-      digest   = OpenSSL::Digest::SHA256.new
-      hmac     = OpenSSL::HMAC.hexdigest(digest, Slanger::Config.secret, payload)
+      digest        = OpenSSL::Digest::SHA256.new
+      hmac          = OpenSSL::HMAC.hexdigest(digest, Slanger::Config.secret, payload)
+      content_type  = 'application/json'
 
       EM::HttpRequest.new(Slanger::Config.webhook_url).
-        post(body: payload, head: { "X-Pusher-Key" => Slanger::Config.app_key, "X-Pusher-Secret" => hmac })
+        post(body: payload, head: { 
+          "X-Pusher-Key" => Slanger::Config.app_key, 
+          "X-Pusher-Signature" => hmac, 
+          "Content-Type" => content_type 
+        })
         # TODO: Exponentially backed off retries for errors
     end
 
